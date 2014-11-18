18fa54ee3a5e1b3b8a45d8ae2258587f6639aec1
Geir Sagberg <geir.sagberg@gmail.com>
2014-11-18 20:47:29 +0100

    First commit

---
+++ RichDomain/Data/MyContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace RichDomain
+{
+	public class MyContext : DbContext
+	{
+		public MyContext ()
+		{
+		}
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+
+		public virtual DbSet<Person> Personer { get; set; }
+
+	}
+
+	public class Person
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public PersonNummer PersonNummer { get; set; }
+	}
+
+	public class PersonNummer
+	{
+
+		public string Value { get; private set; }
+
+		private PersonNummer ()
+		{
+		}
+
+		public PersonNummer (string value) : this ()
+		{
+			if (value.Length != 11)
+				throw new ArgumentException ("PersonNummer must be 11 chars");
+			this.Value = value;
+		}
+
+		public static implicit operator string (PersonNummer personNummer)
+		{
+			return personNummer.Value;
+		}
+
+		public static implicit operator PersonNummer (string str)
+		{
+			return new PersonNummer (str);
+		}
+	}
+}
+
---
+++ RichDomain/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace RichDomain
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			using (var context = new MyContext()) {
+				var person = context.Personer.Create();
+				person.PersonNummer = new PersonNummer("11077941012");
+				context.SaveChanges();
+			}
+
+
+			using (var context = new MyContext()) {
+				var person = context.Personer.First(p => p.PersonNummer == "11077941012");
+				Console.WriteLine(person.Name);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
